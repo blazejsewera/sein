4da5b5fdcede378d1bd9061e7e60799db5ba8167
Blazej Sewera <code@sewera.dev>
2025-04-05 23:42:45 +0200

    feat: add default redirection

--- main.go
+++ main.go
@@ -1,34 +1,75 @@
 package main
 
 import (
-	"fmt"
+	"bytes"
 	"github.com/blazejsewera/sein/monitor"
 	"log/slog"
 	"net/http"
 	"net/url"
+	"text/template"
 )
 
 func main() {
-	fmt.Println("Hello World")
-}
-
-var services = map[string]string{
-	"default": "https://sewera.cc/searxng",
-}
-
-func validateServices() {
-	for _, serviceUrl := range services {
-		if !isParseable(serviceUrl) {
-			monitor.Log().Fatal("cannot parse service URL", slog.String("serviceUrl", serviceUrl))
-		}
+	sr := &serviceResolver{
+		defaultSearchHome: "https://sewera.cc/searxng",
+		defaultSearch:     template.New("https://sewera.cc/searxng?q={{.Query}}"),
+		services:          services(),
+	}
+	sh := &serviceHandler{sr}
+	err := http.ListenAndServe(":8080", sh)
+	if err != nil {
+		monitor.Log().Fatal("cannot start http server", slog.String("err", err.Error()))
 	}
 }
 
-func isParseable(rawUrl string) bool {
-	parsed, err := url.Parse(rawUrl)
-	return err == nil && parsed.Scheme != "" && parsed.Host != ""
+type serviceHandler struct {
+	resolver *serviceResolver
 }
 
-func routeDefault(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, services["default"], http.StatusFound)
+func (s *serviceHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	uri, err := url.ParseRequestURI(r.RequestURI)
+	if err != nil {
+		msg := "cannot parse request URI"
+		monitor.Log().Error(msg, slog.String("uri", r.RequestURI))
+		http.Error(w, msg+": "+r.RequestURI, http.StatusBadRequest)
+		return
+	}
+	query := uri.Query()
+	if !query.Has("q") {
+		http.Redirect(w, r, s.resolver.defaultSearchHome, http.StatusSeeOther)
+	}
+	q := query.Get("q")
+	http.Redirect(w, r, renderTemplateURI(s.resolver.defaultSearch, q), http.StatusSeeOther)
+}
+
+type Query struct {
+	Query string
+}
+
+func renderTemplateURI(t *template.Template, q string) string {
+	b := bytes.Buffer{}
+	err := t.Execute(&b, Query{q})
+	if err != nil {
+		monitor.Log().Error("cannot execute template with query", slog.String("query", q))
+		return ""
+	}
+	return b.String()
+}
+
+var _ http.Handler = new(serviceHandler)
+
+type serviceResolver struct {
+	defaultSearchHome string
+	defaultSearch     *template.Template
+	services          map[string]*template.Template
+}
+
+func services() map[string]*template.Template {
+	wikipedia := template.New("https://en.wikipedia.org/wiki/{{.Query}}")
+	duckduckgo := template.New("https://duckduckgo.com?q={{.Query}}")
+
+	return map[string]*template.Template{
+		"w":   wikipedia,
+		"ddg": duckduckgo,
+	}
 }
